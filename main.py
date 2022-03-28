3396c48fd1bd46fdb61a3ba5d3a26cf475271093
MaxxAferist <maxim.lev.8@mail.ru>
2022-03-28 21:48:01 +0300

    GeniusAPI

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from tokens import BOT_TOKEN
+from tokens import BOT_TOKEN, GENIUS_TOKEN
+import lyricsgenius as lg
 import requests
 
 
@@ -13,10 +14,17 @@ def stop(update, context):
     pass
 
 
+def search_song(update, context):
+    genius = lg.Genius(GENIUS_TOKEN)
+    song = genius.search_song(update.message.text)
+    update.message.reply_text(song.lyrics)
+
+
 def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
+    dp.add_handler(MessageHandler(Filters.text & ~ Filters.command, search_song, pass_user_data=True))
     updater.start_polling()
     updater.idle()
 
---
+++ test.py
@@ -0,0 +1,13 @@
+import lyricsgenius as lg
+from tokens import GENIUS_TOKEN
+import json
+import os
+
+
+genius = lg.Genius(GENIUS_TOKEN)
+song = genius.search_song('Ты способен на подвиг')
+filename = f'lyrics_{song.artist}_{song.title}.json'.lower()
+song.save_lyrics(filename)
+with open(filename) as file:
+    json_song = json.loads(file.read())
+print(json_song)
