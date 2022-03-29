589dbb3992e3c6b25f0e1a5870dd620f6d8f38d3
Artem <mc.Temka2018@mail.ru>
2022-03-29 14:47:10 +0300

    Бот версия 1

--- main.py
+++ main.py
@@ -9,31 +9,66 @@ import spotipy
 import re
 
 
-def start(update, context):
-    pass
-
-
 def stop(update, context):
-    pass
+    update.message.reply_text(
+        "Конец работы"
+    )
+    return ConversationHandler.END
 
 
-def search_song(update, context):
+def process_search(update, context):
+    song_text = update.message.text
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
                                                                client_secret=CLIENT_SECRET))
     genius = lg.Genius(GENIUS_TOKEN)
-    song = genius.search_song(update.message.text)
+    song = genius.search_song(song_text)
     text = f'{song.artist} {song.title}'
     text = re.sub(r'\([^()]*\)', '', text)
     result = sp.search(q=text)['tracks']['items'][0]['external_urls']['spotify']
     update.message.reply_text(song.lyrics)
     update.message.reply_text(f"Spotify: {result}")
+    return ConversationHandler.END
+
+
+def search_song(update, context):
+    answer = update.message.text
+    if answer == 'Поиск':
+        update.message.reply_text(
+            "Введите строку из песни"
+        )
+
+        return 1
+    elif answer == 'Стоп':
+        stop(update, context)
+
+
+def start(update, context):
+    reply_keyboard = [['Поиск'], ['Стоп']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    update.message.reply_text(
+        "Привет, вы попали в MusicBot\n"
+        "С его помощью можно найти любую песню по строке из неё",
+        reply_markup=markup
+    )
 
 
 def main():
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(Filters.text & ~ Filters.command, search_song, pass_user_data=True))
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.text & ~ Filters.command, search_song)],
+
+        states={
+            1: [MessageHandler(Filters.text & ~ Filters.command, process_search, pass_user_data=True)],
+        },
+
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    dp.add_handler(conv_handler)
+    dp.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
     updater.idle()
 
